910229b4dcc17595a59792da753f330ce4be935f
RLT-G <dop.inco.k.b@gmail.com>
2024-11-05 23:21:44 +0300

    1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let wheelCount = 0;
-
+let user_id, link, earned_count, invited_count, rotation_count, balance, username, refer; 
 
 const getParams = (param) => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -41,43 +41,114 @@ function insertData(input, divId) {
 }
 
 
-const getSliceIndex = async () => {
-    try {
-        const response = await fetch("http://192.168.1.102/api/index", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            }
-            // body: JSON.stringify({ name: "John", age: 30 })
-        });
-        if (!response.ok) throw new Error("Ошибка сети: " + response.status);
-        const data = await response.json();
-        console.log(data.sliceIndex)
-        return data.sliceIndex; 
+const openPopup = (msg) => {
+    document.querySelector('.popup').classList.add('active');
+    insertData(msg, 'popup__content_text')
+}
 
-    } catch (error) {
-        return null; 
+
+const closePopup = () => {
+    document.querySelector('.popup').classList.remove('active');
+}
+
+
+const getSliceIndex = async (id) => {
+    const response = await fetch("http://192.168.1.102/api/index", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id: user_id })
+    });
+
+    const data = await response.json();
+    if (data.sliceIndex === undefined || data.sliceIndex === null) {
+        openPopup("У вас недостаточно вращений. Пригласи друга - получи вращение!");
+        return null;
     }
+    return data.sliceIndex;
+};
+
+
+const updateData = () => {
+    insertData(`${String(link)}`, 'link')
+    insertData(`Заработано: ${String(earned_count)}₽`, 'earned_count')
+    insertData(`Приглашено: ${String(invited_count)}`, 'invited_count')
+    insertData(`Вращений: ${String(rotation_count)}`, 'rotation_count')
+    insertData(`Баланс: ${String(balance)}₽`, 'balance')
+    insertData(String(username), 'username')
+    insertData(String(refer) === '0' ? '' : `Пришел от: ${String(refer)}`, 'refer')
 }
 
 
 const init = () => {
     const initValue = getParams("d");
+    console.log(initValue)
     if (initValue){
-        insertData(initValue.link, 'link')
-        insertData(initValue.earned_count, 'earned_count')
-        insertData(initValue.invited_count, 'invited_count')
-        insertData(initValue.rotation_count, 'rotation_count')
-        insertData(initValue.balance, 'balance')
-        insertData(initValue.username, 'username')
-        insertData(initValue.refer, 'refer')
+        user_id = initValue.user_id;
+        link = initValue.link; 
+        earned_count = initValue.earned_count; 
+        invited_count = initValue.invited_count; 
+        rotation_count = initValue.rotation_count; 
+        balance = initValue.balance; 
+        username = initValue.username; 
+        refer = initValue.refer; 
+        updateData();
     }
 
     document.querySelector('.wheel')
         .addEventListener('click', async () => {
             const sliceIndex = await getSliceIndex();
             if (sliceIndex != null && sliceIndex !=undefined) setSlice(sliceIndex);
+            let amount = [15, 100, 5, 25, 250, 10, 50, 500, 1] 
+            console.log(amount[sliceIndex])
+            setTimeout(() => {
+                openPopup(`Поздравляем, вы получили ${amount[sliceIndex]}₽`)
+                earned_count = Number(earned_count) + amount[sliceIndex]
+                balance = Number(balance) + amount[sliceIndex]
+                rotation_count -= 1
+                updateData();
+            }, 17000);
     })
+
+    document.querySelector('.close_popup')
+        .addEventListener('click', () => {
+            closePopup();
+    })
+
+    document.querySelector('.popup')
+        .addEventListener('click', (event) => {
+            if (!document.querySelector('.popup__content').contains(event.target)) {
+                closePopup();
+            }
+    });
+    
+    document.querySelector('.links__btn')
+        .addEventListener('click', () => {
+            navigator.clipboard.writeText(link)
+            document.querySelector('.links__btn').classList.add('click')
+            insertData("Ссылка скопирована", 'links__btn')
+            setTimeout(() => {
+                insertData("Пригласи друга - Получи вращение!", 'links__btn')
+                document.querySelector('.links__btn').classList.remove('click')
+            }, 5000)
+        })
+
+    document.querySelector('.info__btn-1')
+        .addEventListener('click', () => {
+            window.location.href = 'https://vk.com/@-227915984-effektivnoe-privlechenie-polzovatelei-i-reklamodatelei-v-bot';
+    })
+
+    document.querySelector('.info__btn-2')
+        .addEventListener('click', () => {
+            window.location.href = 'https://t.me/lolo_key';
+    })
+
+    document.querySelector('.withdrawal__btn')
+        .addEventListener('click', () => {
+            window.location.href = 'https://t.me/lolo_key';
+    })
+
 }
 
 
